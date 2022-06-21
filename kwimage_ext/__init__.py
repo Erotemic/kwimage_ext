b67642018bfc85a441cc7a5490c90cd4abad044e
joncrall <jon.crall@kitware.com>
2022-06-21 10:20:49 -0400

    Start branch for 0.0.2

--- kwimage_ext/__init__.py
+++ kwimage_ext/__init__.py
@@ -1,7 +1,7 @@
 """
 Binary extensions for kwimage
 """
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'Kitware Inc., Jon Crall'
 __author_email__ = 'kitware@kitware.com, jon.crall@kitware.com'
 __url__ = 'https://gitlab.kitware.com/computer-vision/kwimage_ext'
